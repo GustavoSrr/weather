614ffc2b10ab84a2c94e7b6506d64829590879bb
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-23 16:04:31 -0300

    ✨ Usando context no main e mostrando componente se tiver cidade

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 
 import { Header } from './components/Header'
 import { SearchInput } from './components/SearchInput'
-import { WeatherProvider } from './contexts/weather'
+import { WeatherInfo } from './components/WeatherInfo'
+
+import { useWeather } from './hooks/useWeather'
 
 import { Globals } from './styles/globals'
 
 export const App: React.FC = () => {
+  const { city } = useWeather()
+
   return (
-    <WeatherProvider>
-      <div className="App">
-        <Globals />
-        <Header />
-        <SearchInput />
-      </div>
-    </WeatherProvider>
+    <div className="App">
+      <Globals />
+      <Header />
+      <SearchInput />
+      {
+        city ? <WeatherInfo /> : ''
+      }
+    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { WeatherProvider } from './contexts/weather'
 
 import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
