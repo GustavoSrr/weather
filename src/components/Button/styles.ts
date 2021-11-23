ac48bdb32061162b4e8e87e85a6a0a672ca7fcfa
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-23 16:07:58 -0300

    💄 Alterando estilo do button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -7,13 +7,22 @@ export const Container = styled.button`
 
   padding: 10px;
 
-  border: 0;
+  border: 1px solid transparent;
   font-weight: bold;
   border-radius: 50%;
   background-color: var(--primaryColorTransparent);
   cursor: pointer;
+  transition: 0.1s;
 
   svg {
     fill: var(--primaryColor);
+  }
+
+  &:hover {
+    border-color: var(--primaryColorTransparent);
+  }
+
+  &:focus {
+    filter: brightness(0.9);
   }
 `
