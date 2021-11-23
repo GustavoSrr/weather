de69f74b60e9312d05aa9ea39765b2485a2f2494
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-23 16:05:20 -0300

    ✨ Buscando informações da cidade

--- src/contexts/weather.tsx
+++ src/contexts/weather.tsx
@@ -1,9 +1,51 @@
 import React, { createContext, ReactNode, useState } from 'react'
 
+import axios from 'axios'
+
+export type apiResponseType = {
+  coord: {
+    lon: number,
+    lat: number
+  },
+  weather: [
+    {
+      id: any,
+      main: string,
+      description: string,
+      icon: any
+    }
+  ],
+  main: {
+    temp: number,
+    // eslint-disable-next-line camelcase
+    feels_like: number
+    // eslint-disable-next-line camelcase
+    temp_min: number,
+    // eslint-disable-next-line camelcase
+    temp_max: number,
+    pressure: number,
+    humidity: number
+  },
+  visibility: number,
+  wind: {
+    speed: number,
+    deg: number
+  },
+  clouds: {
+    all: number
+  },
+  sys: {
+    country: string,
+  },
+  timezone: number,
+  name: string,
+  cod: number
+}
+
 type weatherContextType = {
   city: string;
   setCity: React.Dispatch<React.SetStateAction<string>>;
-  searchCity: (city: string) => Promise<void>;
+  searchCity: () => Promise<{ data: apiResponseType; }>;
 }
 
 type weatherProviderProps = {
@@ -14,9 +56,16 @@ export const weatherContext = createContext({} as weatherContextType)
 
 export function WeatherProvider (props: weatherProviderProps) {
   const [city, setCity] = useState('')
+  // const [cityData, setCityData] = useState({} as apiResponseType)
 
-  async function searchCity (city: string) {
-    console.log(city)
+  async function searchCity () {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_BR&appid=${import.meta.env.VITE_WEATHER_API}`
+
+    const { data } = await axios.get<apiResponseType>(url)
+
+    return {
+      data: data
+    }
   }
 
   return (
