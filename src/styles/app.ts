ce68220c63d2d718cd249d78a334542cbfa7f621
Gustavo <gustavosrodrigues2000@gmail.com>
2021-11-25 14:43:45 -0300

    💄 Criando estilos específicos para o app

---
+++ src/styles/app.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  gap: 30px;
+
+  margin-top: 80px;
+  margin-bottom: 80px;
+`
